cff971f57750c2da3790619ffecc7eec30576464
Skyler <skyler.m.degrote@outlook.com>
2016-04-11 20:01:42 -0500

    working on characters main page

--- code/client/js/characters.js
+++ code/client/js/characters.js
@@ -1,3 +1,23 @@
 /**
  * Created by Skyler DeGrote on 4/2/16.
  */
+/**
+ * Created by Skyler DeGrote on 3/17/16.
+ */
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+
+    // character boxes at top. click on them and show only that kind of character
+    $("#mainCharacter").click(function(){
+        $(".visible").removeClass("visible");
+        $(this).addClass("visible");
+    });
+
+
+
+});
+
+
+
+
